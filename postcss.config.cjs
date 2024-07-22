daa7f59f9cde8da3bf172e6c96afcfa12d8aba53
Maurice Busch <gherrink@gmail.com>
2024-07-22 08:25:11 +0200

    feat(base): object padding + margin

--- postcss.config.cjs
+++ postcss.config.cjs
@@ -1,4 +1,9 @@
 /** @type {import('postcss-load-config').Config} */
 module.exports = {
-  plugins: [require('postcss-import'), require('postcss-nested'), require('autoprefixer')],
+  plugins: [
+    require('postcss-import'),
+    require('postcss-nested'),
+    require('postcss-extend'),
+    require('autoprefixer'),
+  ],
 }
