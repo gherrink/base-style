248cff3e49273fa8c2b58d279a556c8c9a957e20
Maurice Busch <gherrink@gmail.com>
2024-07-22 06:36:39 +0200

    feat: init UIDoc

--- vite.config.js
+++ vite.config.js
@@ -1,4 +1,5 @@
 /* eslint-disable sort-keys */
+import uidoc from '@ui-doc/vite'
 import { glob } from 'glob'
 import { resolve } from 'path'
 import { defineConfig } from 'vite'
@@ -11,6 +12,9 @@ export default defineConfig({
         main: resolve(__dirname, 'pages/index.html'),
         ...(await glob(resolve(__dirname, 'pages/[a-z0-9-_][a-z0-9-_]*/index.html'))),
       },
+      output: {
+        dir: './dist',
+      },
     },
   },
   resolve: {
@@ -18,4 +22,17 @@ export default defineConfig({
       '@': resolve(__dirname, 'src'),
     },
   },
+  plugins: [
+    uidoc({
+      settings: {
+        generate: {
+          logo: () => 'Vite',
+        },
+        texts: {
+          title: 'Base Styles',
+        },
+      },
+      source: ['src/*/css/**/*.css'],
+    }),
+  ],
 })
