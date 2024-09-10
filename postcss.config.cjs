d0ce7f164e456a749a899f62b50f0477403d5381
Maurice Busch <gherrink@gmail.com>
2024-09-10 08:49:23 +0200

    feat: removed postcss extend plugin

--- postcss.config.cjs
+++ postcss.config.cjs
@@ -1,9 +1,4 @@
 /** @type {import('postcss-load-config').Config} */
 module.exports = {
-  plugins: [
-    require('postcss-import'),
-    require('postcss-nested'),
-    require('postcss-extend'),
-    require('autoprefixer'),
-  ],
+  plugins: [require('postcss-import'), require('postcss-nested'), require('autoprefixer')],
 }
