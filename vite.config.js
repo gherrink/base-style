5340eff3a4621e87a56ab981cebfd5fe0e105a90
Maurice Busch <gherrink@gmail.com>
2024-09-20 09:02:38 +0200

    feat: update ui-doc

--- vite.config.js
+++ vite.config.js
@@ -4,37 +4,61 @@ import { glob } from 'glob'
 import { resolve } from 'path'
 import { defineConfig } from 'vite'
 
-export default defineConfig({
-  root: 'pages',
-  build: {
-    rollupOptions: {
-      input: {
-        style: 'style.css',
-        main: resolve(__dirname, 'pages/index.html'),
-        ...(await glob(resolve(__dirname, 'pages/[a-z0-9-_][a-z0-9-_]*/index.html'))),
-      },
-      output: {
-        dir: './dist',
-      },
-    },
-  },
-  resolve: {
-    alias: {
-      '@': resolve(__dirname, 'src'),
-    },
-  },
-  plugins: [
-    uidoc({
-      settings: {
-        generate: {
-          logo: () => 'Vite',
+export default defineConfig(async ({ command }) => {
+  return {
+    root: 'pages',
+
+    build: {
+      rollupOptions: {
+        input: {
+          style: 'style.css',
+          app: 'app.ts',
+          main: resolve(__dirname, 'pages/index.html'),
+          ...(await glob(resolve(__dirname, 'pages/[a-z0-9-_][a-z0-9-_]*/index.html'))),
         },
-        texts: {
-          title: 'Base Styles',
+        output: {
+          dir: './dist',
         },
       },
-      source: ['src/*/css/**/*.css'],
-      staticAssets: './assets',
-    }),
-  ],
+    },
+
+    resolve: {
+      alias: {
+        '@': resolve(__dirname, 'src'),
+      },
+    },
+
+    plugins: [
+      uidoc({
+        output: {
+          baseUri: command === 'serve' ? undefined : '.',
+        },
+        source: ['src/*/css/**/*.css'],
+        settings: {
+          generate: {
+            logo: () => 'Vite',
+          },
+          texts: {
+            title: 'Base Styles',
+          },
+        },
+        assets: {
+          static: './assets',
+          example: [
+            {
+              name: 'style',
+              fromInput: true,
+            },
+            {
+              name: 'app',
+              fromInput: true,
+              attrs: {
+                type: 'module',
+              },
+            },
+          ],
+        },
+      }),
+    ],
+  }
 })
