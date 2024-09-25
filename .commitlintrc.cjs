86edb9e3b72089ad84802d6d8a9d2e76d3372eae
Maurice Busch <gherrink@gmail.com>
2024-09-25 09:28:34 +0200

    feat(accordion): styling

--- .commitlintrc.cjs
+++ .commitlintrc.cjs
@@ -1,6 +1,17 @@
 module.exports = {
   extends: ['@commitlint/config-conventional'],
   rules: {
-    'scope-enum': [2, 'always', ['docs', 'pages', 'base', 'elevate', 'image-copyright', 'grid-auto', 'nav-burger-control', 'nav-main', 'shadow']],
+    'scope-enum': [2, 'always', [
+      'docs',
+      'pages',
+      'accordion',
+      'base',
+      'elevate',
+      'image-copyright',
+      'grid-auto',
+      'nav-burger-control',
+      'nav-main',
+      'shadow',
+    ]],
   },
 }
